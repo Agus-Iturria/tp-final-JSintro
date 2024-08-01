8f8b6e615e03271d70d78cca37c7f9be5217c0f2
Agus-Iturria <agustin.iturria@gm2dev.com>
2024-08-01 11:09:48 -0300

    feat(index.html): add verify data exercise

--- tp-final.js
+++ tp-final.js
@@ -1,74 +1,126 @@
-//EJERCICIO INTEGRADOR
-function greetCustomer(name){
-    return("Hola! " + name)
+function greetCustomer(name) {
+    return "Hola! " + name;
 }
-function adultCustomer(age){
-    if (age < 18){
-        return("No esta permitido que usted beba alcohol")
-    }
-    else{
-        return("Usted puede beber alcohol")
+
+function adultCustomer(age) {
+    if (age < 18) {
+        return "No está permitido que usted beba alcohol.";
+    } else {
+        return "Usted puede beber alcohol.";
     }
 }
-function totalOrderCost(product,quantity){
-    let totalCost = 0
-    switch(product){
+
+function totalOrderCost(product, quantity) {
+    let totalCost = 0;
+    switch (product) {
         case "manzana":
-             totalCost = (quantity * 20);
-             return(totalCost);
-             break;
-
-        case "naranja":
-            totalCost = (quantity * 15);
-            return(totalCost);
+            totalCost = quantity * 20;
             break;
-
+        case "naranja":
+            totalCost = quantity * 15;
+            break;
         case "durazno":
-            totalCost = (quantity * 40);
-            return(totalCost);
+            totalCost = quantity * 40;
             break;
         default:
-            return totalCost = 0
+            totalCost = 0;
     }
+    return totalCost;
 }
-function applyDiscount(age,totalCost){
-    if (age > 60){
-        discount = totalCost *= 0.10
-        return discount
+
+function applyDiscount(age, totalCost) {
+    if (age > 60) {
+        return totalCost * 0.10;
     }
+    return 0;
 }
-function determinePayment(payment,totalCost){
+
+function determinePayment(payment, totalCost) {
     if (payment === 'efectivo') {
-        discount = totalCost *= 0.10;
-        return discount
+        return totalCost * 0.10;
     }
+    return 0;
 }
-function orderTime(hour){
+
+function orderTime(hour) {
     if (hour >= 12 && hour <= 14) {
-        return("Es hour pico, puede haber demoras en el servicio.");
+        return "Es hora pico, puede haber demoras en el servicio.";
     }
+    return "";
 }
-function calculateCutlery(totalCost){
-    let impuesto = 0
-    impuesto = totalCost * 0.05; 
-    return impuesto;
+
+function calculateCutlery(totalCost) {
+    return totalCost * 0.05;
 }
+
 function gestionarPedido(name, age, product, quantity, payment, hour) {
     let totalCost = totalOrderCost(product, quantity);
     let ageDiscount = applyDiscount(age, totalCost);
-    let paymentDiscount = determinePayment(payment,totalCost)
+    let paymentDiscount = determinePayment(payment, totalCost);
     let cutleryCost = calculateCutlery(totalCost);
-    let finalCost = totalCost - ageDiscount - paymentDiscount + cutleryCost
+    let finalCost = totalCost - ageDiscount - paymentDiscount + cutleryCost;
 
+    console.log(greetCustomer(name));
+    console.log(adultCustomer(age));
 
-    console.log(`${greetCustomer(name)}`);
-    console.log(`${adultCustomer(age)}`);
-
-    if (totalCost == 0){
-        console.log("producto no disponible");
-    }
-    else{
-        console.log(`Costo total inicial: $${totalCost} \nDescuentos edad: $${ageDiscount} \nMetodo de pago: ${payment} \nDescuentos efectivo: $${(paymentDiscount)} \nImpuesto: $${(cutleryCost)} \nCosto Final: $${finalCost} \n${orderTime(hour)}`);
+    if (totalCost == 0) {
+        console.log("Producto no disponible.");
+    } else {
+        console.log(`Costo total inicial: $${totalCost} \nDescuento por edad: $${ageDiscount} \nMétodo de pago: ${payment} \nDescuento por efectivo: $${paymentDiscount} \nImpuesto: $${cutleryCost} \nCosto final: $${finalCost} \n${orderTime(hour)}`);
     }
 }
-gestionarPedido("José", 63, "naranja", 1, "efectivo", 12);
+
+function solicitarDatos(){
+    let name = prompt("Ingrese el nombre del cliente:");
+    if ((name.length) > 30 || (name.length) <= 0){
+        console.log("Nombre no valido")
+    }
+    console.log("Nombre: " + name);
+
+    let age = prompt("Ingrese la edad del cliente:");
+    if (isNaN(age) || age <= 0) {
+        console.log("Edad no valida");
+        return
+    }
+    else{
+        console.log("Edad: " + age);
+    }
+
+    let product = prompt("Ingrese el producto que desea comprar (manzana, naranja, durazno):");
+    if (product == "manzana" || product == "naranja" || product == "durazno"){
+        console.log("Producto: " + product);
+    }
+    else{
+        console.log("Producto no valido");
+        return
+    }
+
+    let quantity = prompt("Ingrese la cantidad a comprar:");
+    if (isNaN(quantity) || quantity <= 0) {
+        console.log("Cantidad no valida");
+        return
+    }
+    else{
+        console.log("Cantidad: "+ quantity);
+    }
+
+    let payment = prompt("Ingrese el método de pago (efectivo, tarjeta):");
+    if (payment == 'efectivo' || payment == 'tarjeta') {
+        console.log("Metodo de Pago: "+ payment);
+        }
+    else{
+        console.log("Método de pago no valido");
+        return
+    }
+
+    let hour = prompt("Ingrese la hora del pedido (en formato 24 horas):");
+    if (isNaN(hour) || hour < 0 || hour > 23) {
+        console.log("Hora no valida");
+        return
+    }
+    else{
+        console.log("Hora del pedido: "+ hour);
+    }
+    gestionarPedido(name, age, product, quantity, payment, hour);
+}
+solicitarDatos();
